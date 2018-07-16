348985ffd35bac024e5843d399033c7807599ee1
omfg <podsh74@gmail.com>
2018-07-16 16:07:06 +0500

    done ver 0.2.1 - Basic authorisation in ISP Bill Manager - Returnes api auth key - Uses retrofit 2 - Supports saving user data to file and loading it automatically

--- src/main/java/ISPMainAuth.java
+++ src/main/java/ISPMainAuth.java
@@ -6,14 +6,44 @@ import java.io.*;
 
 public class ISPMainAuth {
     public String authorize(String filename){
+
         String key = "";
         File file = new File(filename);
+        if(file.exists()) {
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(filename);
+                ObjectInputStream objectInputStream = new ObjectInputStream(fis);
+                LoginPassword loginPassword = (LoginPassword) objectInputStream.readObject();
+                key = getApiKey(loginPassword);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }else {
+            LoginPassword loginPassword = new LoginPassword();
+            Auth auth = new Auth();
+            loginPassword = auth.askLoginPass();
+            FileWorker fileWorker = new FileWorker("user_data.sav",loginPassword);
+            fileWorker.writeToFile();
+            key = getApiKey(loginPassword);
+            System.out.println("KEY "+key);
+
+        }
+        return key;
+    }
+    public LoginPassword readLoginPassword(String filename){
         FileInputStream fis = null;
+        LoginPassword loginPassword = new LoginPassword();
         try {
             fis = new FileInputStream(filename);
             ObjectInputStream objectInputStream = new ObjectInputStream(fis);
-            LoginPassword loginPassword = (LoginPassword) objectInputStream.readObject();
-            key = getApiKey(loginPassword);
+           loginPassword = (LoginPassword) objectInputStream.readObject();
+
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -22,8 +52,7 @@ public class ISPMainAuth {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-
-        return key;
+        return loginPassword;
     }
 
     public  String  authorize(boolean isAuthorized){
